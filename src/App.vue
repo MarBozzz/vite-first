ce533eaca11bc51baa277c7dbb1b7a6e68535b71
MarBozzz <mbozzuto@libero.it>
2022-11-14 17:15:57 +0100

    [created] component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  name : 'App',
+  components : {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name : 'AppTitle',
+  data(){
+    return{
+      titolo: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="container">
+    <h1>{{titolo}}</h1>
+  </div>
+</template>
+
+
+<style>
+
+</style>
